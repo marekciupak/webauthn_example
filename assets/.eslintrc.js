741c48917790e3ec3f03af919a87d83c88b40ef3
Marek Ciupak <6394528+marekciupak@users.noreply.github.com>
2021-08-17 23:25:16 +0200

    Add linters

---
+++ assets/.eslintrc.js
@@ -0,0 +1,25 @@
+/* eslint-env node */
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended", "prettier"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
